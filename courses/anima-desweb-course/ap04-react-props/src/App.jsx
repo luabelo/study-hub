63dca66660e162548675a1535edc5b5233b8dffd
Luana Belo <luanabelo45@gmail.com>
2025-09-18 21:59:09 -0300

    feat: adiciona tela de pedidos com feedback

--- courses/anima-desweb-course/ap04-react-props/src/App.jsx
+++ courses/anima-desweb-course/ap04-react-props/src/App.jsx
@@ -1,37 +1,108 @@
+import Pedido from "./Pedido"
+import Animal from "./Animal"
+import Cartao from "./Cartao"
+import Feedback from "./Feedback"
+
 const App = () => {
-  return(
-    <div className="container-fluid p-3">
+  const funcaoOK = () => alert = ('Agradecemos o Feedback')
+  const funcaoNOK = () => alert = ('Verificaremos')
+  const textoOK = 'Pedido já chegou'
+  const textoNOK = 'Ainda não chegou'
+  const pedidos =[
+    { 
+      data: '03/09/2025',
+      icone: 'car',
+      titulo: 'Carro',
+      descricao: 'Porsche Amarelo'
+    },
+    {
+      data: '05/09/2025',
+      icone: 'plane',
+      titulo: 'Avião',
+      descricao: 'Avião Amarelo'
+    },
+    {
+      data: '07/09/2025',
+      icone: 'building',
+      titulo: 'Prédio',
+      descricao: 'Prédio Amarelo'
+    },
+    {
+      data: '09/09/2025',
+      icone: 'computer',
+      titulo: 'Computador',
+      descricao: 'Computador Amarelo'
+    }
+  ]
+  const expressaoFeedback = (
+    <Feedback
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+      textoOK={textoOK}
+      textoNOK={textoNOK} />
+  )
+  return (
+    <div className="container-fluid   p-5" style={{ backgroundColor: 'grey' }}>
       <div className="row">
         <div className="col-12">
-          <i className="fa-solid fa-3x fa-cat" style={{color: "#b5ca8d"}}></i>
+          <Cartao cabecalho="Gatopótamos">
+            <div className="d-flex-flex-column">
+              <div className="d-flex justify-content-center align-items-end mb-5">
+                <Animal
+                  animal="cat"
+                  tamanho="3"
+                  espelhamento="1"
+                />
+                <Animal
+                  animal="cat"
+                  tamanho="2"
+                  espelhamento="-1"
+                />
+                <Animal
+                  animal="cat"
+                  tamanho="1"
+                  espelhamento="-1"
+                />
+                <Animal
+                  animal="cat"
+                  tamanho="1"
+                  espelhamento="-1"
+                />
+                <Animal
+                  animal="hippo"
+                  tamanho="1"
+                  espelhamento="-1"
+                />
+              </div>
+              <Feedback
+                textoOK="Adotar um furão?"
+                textoNOK="Não adotar!"
+                funcaoOK={() => alert('Oba!')}
+                funcaoNOK={() => alert(':(')} />
+            </div>
+          </Cartao>
         </div>
       </div>
-      <div className="row">
-        <div className="col-sm-12 col-md-6 col-lg-3">
-          <div className="card" style={{borderColor: "#b5ca8d"}}>
-            <div className="card-header text-muted" style={{backgroundColor: "#b5ca8d"}}>
-              04/09/2025
+      <div className="row mt-2">
+        {
+          pedidos.map(({data, icone, titulo, descricao}) => {
+            return(
+            <div className="col-sm-12 col-md-6 col-xl-4">
+              <Cartao
+                cabecalho={data}>
+                <Pedido
+                  icone={icone}
+                  titulo={titulo}
+                  descricao={descricao}/>
+                <div className="mt-3"> </div>
+                {expressaoFeedback}
+              </Cartao>
             </div>
-            <div className="card-body d-flex">
-              <i className="fa-solid fa-2x fa-bowl-food" style={{color: "#b5ca8d"}}></i>
-                <div>
-                  <p>Ração Seca</p>
-                  <p>Salmão</p>
-                </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-3">
-
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-3">
-
-        </div>
-        <div className="col-sm-12 col-md-6 col-lg-3">
-        </div>
+            )
+          })
+        }
       </div>
     </div>
   )
 }
-
 export default App
---
+++ courses/anima-desweb-course/ap04-react-props/src/Feedback.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Feedback = ({textoOK, textoNOK, funcaoOK, funcaoNOK}) => {
+  return (
+    <div className='d-flex justify-content-evenly'>
+        <button 
+            type="button" 
+            onClick={funcaoOK}
+            className="btn btn-primary">
+                {textoOK}
+        </button>
+        <button 
+            type="button" 
+            onClick={funcaoNOK}
+            className="btn btn-danger">
+                {textoNOK}
+        </button>
+    </div>
+  )
+}
+
+export default Feedback
