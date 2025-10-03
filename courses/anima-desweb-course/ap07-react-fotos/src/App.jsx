6ce346a6806f7416316b6502ea227268c3b4153f
Luana Belo <luanabelo45@gmail.com>
2025-10-02 21:21:07 -0300

    feat(course): inicia apostila 07

---
+++ courses/anima-desweb-course/ap07-react-fotos/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from 'primereact/button'
+
+const App = () => {
+  return(
+    <div>
+      <div>
+        <i className="pi pi-car"></i>
+      </div>
+      <Button label='Testando'/>
+    </div>
+  )
+}
+
+export default App
---
+++ courses/anima-desweb-course/ap07-react-fotos/src/components/Busca.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Busca extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>Busca</div>
+    )
+  }
+}
