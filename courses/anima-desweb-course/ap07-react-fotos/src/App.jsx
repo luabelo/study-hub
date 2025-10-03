1d2becbc4261fc62a81b54c7754240ba1b1a8d0f
Luana Belo <luanabelo45@gmail.com>
2025-10-02 21:46:05 -0300

    feat(course): adiciona busca com botão

--- courses/anima-desweb-course/ap07-react-fotos/src/App.jsx
+++ courses/anima-desweb-course/ap07-react-fotos/src/App.jsx
@@ -1,12 +1,16 @@
 import { Button } from 'primereact/button'
+import Busca from './components/Busca'
 
 const App = () => {
   return(
     <div>
-      <div>
+      <div className='flex mb-3'>
+        <i className="pi pi-car mr-3"></i>
         <i className="pi pi-car"></i>
       </div>
-      <Button label='Testando'/>
+      <div>
+        <Busca />
+      </div>
     </div>
   )
 }
