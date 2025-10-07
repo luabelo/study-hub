cf04b191bdf1a2256372cf1e780b3e4147decb7b
Luana Belo <luanabelo45@gmail.com>
2025-10-06 21:47:35 -0300

    feat(course): implementa endpoints de observacoes

---
+++ courses/anima-sdmobile-course/microsservicos/barramento/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import axios from 'axios'
+const app = express()
+// middleware
+app.use(express.json())
+
+app.post('/eventos', async (req, res) => { 
+    const evento = req.body
+    console.log(evento)
+    axios.post('http://localhost:4000/eventos', evento)
+    axios.post('http://localhost:5000/eventos', evento)
+    res.end()
+})
+
+const port = 10000
+app.listen(port, () => console.log(`Barramento. Porta ${port}.`))
--- courses/anima-sdmobile-course/microsservicos/observacoes/index.js
+++ courses/anima-sdmobile-course/microsservicos/observacoes/index.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const app = express()
+const {v4: uuidv4} = require('uuid')
 app.use(express.json())
 
 const baseObservacoes = {}
 app.post('/lembretes/:id/observacoes', (req, res) => {
-
+    const idObs = uuidv4()
+    const {texto} = req.body
+    const observacao = {
+        id: idObs,
+        texto
+    }
+    const observacoes = baseObservacoes[req.params.id] || []
+    observacoes.push(observacao)
+    baseObservacoes[req.params.id] = observacoes
+    res.status(201).json(observacao)
 })
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.json(baseObservacoes[req.params.id] || [])
+})
 
+app.delete('/lembretes/:id/observacoes/:idObs', (req, res) => {
+    const {id, idObs} = req.params
+    const observacoes = baseObservacoes[id] || []
+    const indice = observacoes.findIndex(o => o.id === idObs)
+    if (indice >= 0) {
+        observacoes.splice(indice, 1)
+        baseObservacoes[id] = observacoes
+        res.status(204).send()
+    } else {
+        res.status(404).json({mensagem: 'Observação não encontrada.'})
+    }
 })
 
 const port = 5000
